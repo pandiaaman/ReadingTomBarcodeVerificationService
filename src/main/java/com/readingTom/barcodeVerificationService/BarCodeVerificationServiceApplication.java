9178bf703729ad48365f7816ab5bca33c34d908b
Aman Pandia <pandiaaman16@gmail.com>
2024-04-27 18:15:34 +0530

    first commit

---
+++ src/main/java/com/readingTom/barcodeVerificationService/BarCodeVerificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.readingTom.barcodeVerificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarCodeVerificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarCodeVerificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readingTom/barcodeVerificationService/BarCodeVerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readingTom.barcodeVerificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarCodeVerificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
